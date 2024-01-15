b720edeeb9951a9046307d88e010265ee7332d40
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-15 11:41:26 +0200

    list grid

--- src/components/persons/PersonList.jsx
+++ src/components/persons/PersonList.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+import { personData } from './data';
+
 export default function PersonList() {
+  // console.log('personData ===', personData);
+  const [mainArr, setMainArr] = useState(personData);
+
+  // setTimeout(() => {
+  //   setMainArr(personData); // infinite loop
+  // }, 3000);
+  console.log('mainArr ===', mainArr);
   return (
     <div>
       <h2>Persons</h2>
-      <ul className='row'>
-        <li className='column column-50'>
-          <h3>Person name</h3>
-          <p>person age</p>
-        </li>
+      <ul className='grid'>
+        {mainArr.map((pObj) => (
+          <li key={pObj.id} className='counter'>
+            <h3>{pObj.name}</h3>
+            <p>Age:{pObj.age}</p>
+            <p>Gender: {pObj.gender}</p>
+            <p>Hobby: {pObj.hobby}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
