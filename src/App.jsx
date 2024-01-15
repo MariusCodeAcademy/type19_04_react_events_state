ff833c4d32f1d1a45fd6ad8d491fa7ba8630bb78
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-15 09:15:41 +0200

    up works

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Counter from './components/counters/Counter';
 import './styles/App.css';
 
 export default function App() {
+  console.log('<App />');
   return (
     <div className='App container'>
       <h1>Hello, world!</h1>
--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+
 export default function Counter() {
+  console.log('<Counter /> sukurtas');
+  let value = 0;
+  const [counterValue, setCounterValue] = useState(0);
+  // counterValue += 1; // niekada nekeiciam counterValue tiesiogiai su prilyginimu
+  // console.log('counterValue ===', counterValue);
+  console.log('value ===', value);
+
+  function up() {
+    console.log('up fired');
+    value++;
+    console.log('value inside ===', value);
+    // state setinimo funkcija visada perkrauna komponenta
+    setCounterValue(counterValue + 1);
+    // setCounterValue(counterValue++);
+    console.log('counterValue inside ===', counterValue);
+  }
+
   return (
     <div className='counter'>
-      <p className='value'>0</p>
+      <p className='value'>{value}</p>
+      <p className='value'>{counterValue}</p>
       <div className='control'>
-        <button>Up</button>
+        <button onClick={up}>Up</button>
       </div>
     </div>
   );
