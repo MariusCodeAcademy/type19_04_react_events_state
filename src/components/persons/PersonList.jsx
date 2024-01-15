1daf9c3b47b67dfb1d1ffe3b1bb58c408a174614
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-15 11:52:03 +0200

    uzd

--- src/components/persons/PersonList.jsx
+++ src/components/persons/PersonList.jsx
@@ -5,13 +5,26 @@ export default function PersonList() {
   // console.log('personData ===', personData);
   const [mainArr, setMainArr] = useState(personData);
 
-  // setTimeout(() => {
-  //   setMainArr(personData); // infinite loop
-  // }, 3000);
   console.log('mainArr ===', mainArr);
+
+  const howMany = mainArr.length;
+
+  const malesCount = mainArr.filter((pObj) => pObj.gender === 'male').length;
+  console.log('malesCount ===', malesCount);
   return (
     <div>
-      <h2>Persons</h2>
+      <h2>Persons ({howMany})</h2>
+      <fieldset>
+        <legend>Info</legend>
+        <p>Total persons: {howMany}</p>
+        <p>Total males: {malesCount}</p>
+        <p>Total females: </p>
+        <p>Persons above 37 years: </p>
+        <p>Person with longesnt name: </p>
+        <p>Average persons age: </p>
+        <p>Total persons age: </p>
+        <p>(all hobies atskirti kableliais ir tarpeliais)</p>
+      </fieldset>
       <ul className='grid'>
         {mainArr.map((pObj) => (
           <li key={pObj.id} className='counter'>
