b0ea9c6e9e736f5c57ff0853d02506e00451b9ef
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-15 09:17:02 +0200

    uzd

--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react';
 
 export default function Counter() {
   console.log('<Counter /> sukurtas');
-  let value = 0;
+  // let value = 0;
   const [counterValue, setCounterValue] = useState(0);
   // counterValue += 1; // niekada nekeiciam counterValue tiesiogiai su prilyginimu
   // console.log('counterValue ===', counterValue);
-  console.log('value ===', value);
+  // console.log('value ===', value);
 
   function up() {
     console.log('up fired');
-    value++;
-    console.log('value inside ===', value);
+    // value++;
+    // console.log('value inside ===', value);
     // state setinimo funkcija visada perkrauna komponenta
     setCounterValue(counterValue + 1);
     // setCounterValue(counterValue++);
@@ -20,10 +20,12 @@ export default function Counter() {
 
   return (
     <div className='counter'>
-      <p className='value'>{value}</p>
+      {/* <p className='value'>{value}</p> */}
       <p className='value'>{counterValue}</p>
       <div className='control'>
         <button onClick={up}>Up</button>
+        <button>Down</button>
+        <button>up by 10</button>
       </div>
     </div>
   );
